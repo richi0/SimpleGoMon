3dc8c2a2a95301c69d87377a3bcd87f410e36eba
Richi0 <csspliskin@hotmail.com>
2022-05-05 21:18:16 +0200

    readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple gomon
+Simple gomon monitors your project directory and runs commands if it detects changes.
+
+## Usage of ./gomon:
+- build string
+    - Custom build command (default "go build -o=__temp__")
+- run string
+    - Custom run command (default "./__temp__")
+- tearDown string
+    - Custom teadown command (default "rm __temp__")
+- types string
+    - File types to monitor (default "go,htmp,css,js,tmpl")
+
+Only the files defined in `types` (default "go,htmp,css,js,tmpl") are monitored. At startup and if a change is detected the build command and the run command are executed. When the program is stopped the teardown command is executed to clean up.
--- main.go
+++ main.go
@@ -72,69 +72,58 @@ func isEqual(pattern string) bool {
 	return true
 }
 
-func runCommand(build string, run string) *exec.Cmd {
-	var temp *exec.Cmd
+func makeCmd(c string) *exec.Cmd {
 	var cmd *exec.Cmd
-	b := strings.Split(build, " ")
-	if len(b) > 1 {
-		temp = exec.Command(b[0], b[1:]...)
-	} else if len(b) == 1 {
-		temp = exec.Command(build)
+	command := strings.Split(c, " ")
+	if len(command) > 1 {
+		cmd = exec.Command(command[0], command[1:]...)
+	} else if len(command) == 1 {
+		cmd = exec.Command(c)
 	} else {
-		log.Panic("Build command error", build)
+		log.Panic("command error", c)
 	}
-	temp.Run()
-	r := strings.Split(run, " ")
-	if len(r) > 1 {
-		cmd = exec.Command(r[0], r[1:]...)
-	} else if len(r) == 1 {
-		cmd = exec.Command(run)
-	} else {
-		log.Panic("Run command error", run)
-	}
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Start()
 	return cmd
+}
+
+func rebuild(build string, run string) *exec.Cmd {
+	buildCmd := makeCmd(build)
+	buildCmd.Run()
+	runCmd := makeCmd(run)
+	runCmd.Stdout = os.Stdout
+	runCmd.Stderr = os.Stderr
+	runCmd.Start()
+	return runCmd
 }
 
 func main() {
 	buildCmd := flag.String("build", "go build -o=__temp__", "Custom build command")
 	runCmd := flag.String("run", "./__temp__", "Custom run command")
-	tearDownCmd := flag.String("tearDown", "rm __temp__", "Custom tear down command")
-	fileTypes := flag.String("types", "go,htmp,css,js,tmpl", "File types to check for changes, Example: -types=go,html,js ")
+	tearDownCmd := flag.String("tearDown", "rm __temp__", "Custom teardown command")
+	fileTypes := flag.String("types", "go,htmp,css,js,tmpl", "File types to monitor")
 	flag.Parse()
 
 	// Teardown
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt, os.Kill, syscall.SIGTERM)
 	go func() {
-		var cmd *exec.Cmd
 		signalType := <-ch
 		signal.Stop(ch)
 		fmt.Println("Exit command received. Exiting...", signalType)
-		c := strings.Split(*tearDownCmd, " ")
-		if len(c) > 1 {
-			cmd = exec.Command(c[0], c[1:]...)
-		} else if len(c) == 1 {
-			cmd = exec.Command(*tearDownCmd)
-		} else {
-			log.Panic("Build command error", cmd)
-		}
+		cmd := makeCmd(*tearDownCmd)
 		cmd.Run()
 		os.Exit(0)
 	}()
 
 	pattern := fmt.Sprintf(".+\\.(%s)", strings.ReplaceAll(*fileTypes, ",", "|"))
 	setup(pattern)
-	cmd := runCommand(*buildCmd, *runCmd)
+	cmd := rebuild(*buildCmd, *runCmd)
 	for {
 		time.Sleep(time.Second * 1)
 		if !isEqual(pattern) {
 			fmt.Println("Update detected")
 			setup(pattern)
 			cmd.Process.Kill()
-			cmd = runCommand(*buildCmd, *runCmd)
+			cmd = rebuild(*buildCmd, *runCmd)
 		}
 	}
 }
